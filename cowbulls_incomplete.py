f42f67f3947448046f3d6e123b865afffd69b14c
mohamed <MA3654@live.mdx.ac.uk>
2023-11-22 20:13:49 +0400

    correction

--- cowbulls_incomplete.py
+++ cowbulls_incomplete.py
@@ -1,32 +1,37 @@
 import random
 
 def compare_numbers(number, user_guess):
-    ## your code here
-    return cowbull
+    cow =0
+    bull=0
+    for i in range(4):
+        if user_guess[i] == number[i]:
+            bull+=1
+        elif user_guess[i] in number:
+            cow+=1
+    return cow, bull
 
-playing = True #gotta play the game
-number = str(random.randint(0,9999)) #random 4 digit number
+playing = True  # gotta play the game
+number = str(random.randint(1000, 9999))  # random 4 digit number
 guesses = 0
-print number
 
-print("Let's play a game of Cowbull!") #explanation
+print("Let's play a game of Cowbull!")  # explanation
 print("I will generate a number, and you have to guess the numbers one digit at a time.")
-print("For every number that exists in the sequence but is in wrong place, you get a cow. For every one in the right place, you get a bull.")
+print("For every number that exists in the sequence but is in the wrong place, you get a cow. For every one in the right place, you get a bull.")
 print("The game ends when you get 4 bulls!")
 print("Type exit at any prompt to exit.")
 
 while playing:
-    user_guess = raw_input("Give me your best guess!")
-    if user_guess == "exit":
+    user_guess = input("Give me your best guess!")
+    if user_guess.lower()=="exit":
         break
-    cowbullcount = compare_numbers(number,user_guess)
-    guesses+=1
+    cowbullcount = compare_numbers(number, user_guess)
+    guesses+= 1
 
-    print("You have "+ str(cowbullcount[0]) + " cows, and " + str(cowbullcount[1]) + " bulls.")
+    print(f"You have {cowbullcount[0]} cows, and {cowbullcount[1]} bulls.")
 
     if cowbullcount[1]==4:
         playing = False
-        print("You win the game after " + str(guesses) + "! The number was "+str(number))
-        break #redundant exit
+        print(f"You win the game after {guesses} guesses! The number was {number}")
+        break
     else:
         print("Your guess isn't quite right, try again.")
