08bbd1d218bd6bb89f413d47b36a35a9b5353a16
mohamed <MA3654@live.mdx.ac.uk>
2023-11-28 11:04:26 +0400

    fixed errors

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,8 @@
+#welcoming the user with input
 greeting = input("Hello, possible pirate! What's the password?")
+#result if the user input is the same as the pirate password or not
 if greeting == "Arrr!":
 	print("Go away, pirate.")
+#result if the user is the same as non-pirate password or not
 else:
 	print("Greetings, hater of pirates!")
