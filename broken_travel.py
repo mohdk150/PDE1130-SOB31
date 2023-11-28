b102b371de57ef322682e427d2d9f705158b2809
mohamed <MA3654@live.mdx.ac.uk>
2023-11-28 11:24:50 +0400

    fixed errors of the years

--- broken_travel.py
+++ broken_travel.py
@@ -6,10 +6,13 @@
 # if he is from the distant past (before 1900),
 # the present era (1900-2020) or from the far future (beyond 2020).
 
+#input for the year of origin from the user
 year =int(input("Greetings! What is your year of origin?"))
 
+#checking if the year is in the past or not
 if year <=1900:
     print ("Woah, that's the past!")
+#checking if the year is in the present or not
 elif year > 1900 and year < 2020:
     print ("That's totally the present!")
 else:
